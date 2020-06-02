eabf4bda8168014a7c0cda9c2ff67ad62b9f382a
cbrianfoley <47460320+cbrianfoley@users.noreply.github.com>
2020-06-02 08:31:22 -0500

    Add files via upload

---
+++ UWP-install-and-shortcut.ps1
@@ -0,0 +1,15 @@
+﻿# Trigger install
+Add-AppxProvisionedPackage -Online -PackagePath "\\server\path\appname.AppxBundle" -LicensePath "\\server\path\applicense.xml"
+
+# Wait 1 minutes for it to install
+Start-Sleep -Seconds 60
+
+# Create desktop shortcut
+$Appxpackage = get-appxpackage | where Name -EQ "InnovativeSystemsLLC.eLationReach"
+$TargetFile = "C:\Windows\explorer.exe"
+$TargetPath =  "shell:AppsFolder\" + $Appxpackage.PackageFamilyName + "!App"
+$ShortcutFile = "C:\Users\Public\Desktop\eLation Reach.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetPath
+$Shortcut.Save()
